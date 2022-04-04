c7123659d5dd2e14d86508e5fd2a2d68616e2c74
LESAGE Gabriel <gabriel.lesage@alumni.univ-avignon.fr>
2022-04-04 10:42:32 +0200

    Implementation du multithreading JavaFX

--- src/main/java/ablw/projetprototypageinterfaceablw/RechercheVoyage.java
+++ src/main/java/ablw/projetprototypageinterfaceablw/RechercheVoyage.java
@@ -1,9 +1,46 @@
 package ablw.projetprototypageinterfaceablw;
 
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
 import java.util.List;
 
-public class RechercheVoyage {
-    public Sejour[] rechercheSejour(Sejour[] listeSejour, String recherche){
+public class RechercheVoyage extends Service<Sejour[]> {
+
+    Sejour[] listeSejour;
+    String recherche;
+
+    public RechercheVoyage(Sejour[] listeSejour, String recherche) {
+        this.listeSejour = listeSejour;
+        this.recherche = recherche;
+    }
+
+    public void setListeSejour(Sejour[] listeSejour) {
+        this.listeSejour = listeSejour;
+    }
+
+    public Sejour[] getListeSejour() {
+        return listeSejour;
+    }
+
+    public String getRecherche() {
+        return recherche;
+    }
+
+    protected Task<Sejour[]> createTask(){
+        return new Task<Sejour[]>() {
+            @Override
+            protected Sejour[] call() throws Exception {
+                return rechercheSejour(getListeSejour(), getRecherche());
+            }
+        };
+    }
+
+    public  Sejour[]rechercher(Sejour[] listeSejour, String recherche){
+
+    }
+
+    private Sejour[] rechercheSejour(Sejour[] listeSejour, String recherche){
         if(recherche.length() < 2) {
             return null;
         }
