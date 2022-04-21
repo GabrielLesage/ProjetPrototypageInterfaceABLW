a91e46bd8a1f0b3c3d6c328acfa0bc2d8d09bf61
Viringe <sora30200@gmail.com>
2022-04-21 16:43:14 +0200

    edit css
    recycler view
    ligne like netflix
    edit fxml

--- src/main/java/ablw/projetprototypageinterfaceablw/HelloController.java
+++ src/main/java/ablw/projetprototypageinterfaceablw/HelloController.java
@@ -1,14 +1,90 @@
 package ablw.projetprototypageinterfaceablw;
 
+import javafx.beans.NamedArg;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
+import javafx.util.Callback;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Vector;
+
+public class HelloController implements Initializable {
+
+    //class to contain objet of data for a ligne
+    protected static class CustomPanel {
+        protected String titre;
+        protected int nb_element;
+
+        public String getTitre() {
+            return titre;
+        }
+
+        public void setTitre(String titre) {
+            this.titre = titre;
+        }
+
+        public int getNb_element() {
+            return nb_element;
+        }
+
+        public void setNb_element(int nb_element) {
+            this.nb_element = nb_element;
+        }
+
+        public CustomPanel(String titre, int nb_element) {
+            super();
+            this.titre = titre;
+            this.nb_element = nb_element;
+        }
+    }
+
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+    @FXML
+    ListView<CustomPanel> myListView;
+    Vector<CustomPanel> customPanel;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        this.customPanel = new Vector<>();
+
+        //need to get data
+
+        for (int i = 0; i < 1000; i++) { //2500
+            CustomPanel paneSectionVar = new CustomPanel("test", 10);
+            customPanel.add(paneSectionVar);
+        }
+
+        myListView.getItems().addAll(customPanel);
+
+        myListView.setCellFactory(new Callback<ListView<CustomPanel>, ListCell<CustomPanel>>() {
+            @Override
+            public ListCell<CustomPanel> call(ListView<CustomPanel> listView) {
+                return new PaneSection();
+            }
+        });
+
     }
 }
