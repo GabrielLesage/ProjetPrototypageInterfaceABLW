2766fec55e49cddc101ebaad2650a219c3a9b86f
de2u <45523885+de2u@users.noreply.github.com>
2022-04-26 00:46:45 +0200

    Classes Messagerie

---
+++ src/main/java/ablw/projetprototypageinterfaceablw/Message.java
@@ -0,0 +1,27 @@
+package ablw.projetprototypageinterfaceablw;
+
+public class Message {
+    private String auteur;
+    private String contenu;
+
+    public Message(String auteur, String contenu) {
+        this.auteur = auteur;
+        this.contenu = contenu;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    public String getContenu() {
+        return contenu;
+    }
+
+    public void setContenu(String contenu) {
+        this.contenu = contenu;
+    }
+}
